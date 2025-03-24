059a0d6b1e575e4345569a4dba07351ae690cf79
KadolValery <valerakadol0207@gmail.com>
2025-03-24 18:12:02 +0300

    django_proj_hw_2

--- src/core/urls.py
+++ src/core/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # path('admin/', admin.site.urls),
+    # path('shop/<int:year>/', include('shop.urls')),
     path('admin/', admin.site.urls),
-    path('shop/<int:year>/', include('shop.urls')),
+    path('shop/', include('shop.urls'))
 
 ]
--- src/shop/views.py
+++ src/shop/views.py
@@ -2,21 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def first_views(request,year):
-    return HttpResponse(f"<h1>Это главная страница, где будет отображаться информация о сайте и задача сервиса{year}<h/1>")
+# def first_views(request,year):
+#     return HttpResponse(f"<h1>Это главная страница, где будет отображаться информация о сайте и задача сервиса{year}<h/1>")
+#
+# def second_views(request,year):
+#     return HttpResponse(f"<h1>Это страница информации где будут в дальнейшем выводиться все товары маркетплейса{year}<h/1>")
+#
+# def first_html(request):
+#     data={
+#         "user":"Ivan",
+#         "age": 24,
+#     }
+#
+#     context={
+#         "data": data,
+#         "var_str": "Hello context"
+#     }
+#
+#     return render(request, template_name='hello.html',context=context)
+def home(request):
+    return render(request, template_name="home.html")
 
-def second_views(request,year):
-    return HttpResponse(f"<h1>Это страница информации где будут в дальнейшем выводиться все товары маркетплейса{year}<h/1>")
+def info(request):
+    return render(request, template_name="info.html")
 
-def first_html(request):
-    data={
-        "user":"Ivan",
-        "age": 24,
-    }
+def products(request):
+    return render(request, template_name="products.html")
 
-    context={
-        "data": data,
-        "var_str": "Hello context"
-    }
-
-    return render(request, template_name='hello.html',context=context)
+def users(request):
+    return render(request,template_name="users.html")
